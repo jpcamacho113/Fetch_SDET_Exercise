a69da6f277c297801a417e55a6643fdf9e253128
Juan <juanpc2@uic.edu>
2023-10-12 00:07:59 -0500

    Logic, comments, and readme. Dependency changes

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Fetch_SDET_Exercise
 
+## Requirements
 Java install link: https://www.java.com/en/download/manual.jsp
-Install mave, java.
+Maven download link: https://maven.apache.org/download.cgi
+Maven installation link: https://maven.apache.org/install.html
+
+Any necessary jar files for the Java classpath have been included.
+
+## Running the application
+
+### Terminal
+Navigate to the Fetch_SDET_Exercise directory after obtaining the necessary files and run the below command:
+*java -cp selenium-java-4.14.0\lib\* .\scale\src\main\java\com\weightscale\ScaleClass.java*
+
+### IDE
+The application should function similar to any other obtained from Github.
+
+## The Algorithm
+
+The way I chose to find the fake gold bar is by seperating the 9 possibilities into 3 groups. I first compare the weight of the first two groups (group1 and group2 var names). If group1 weighs less than group2 then I know group1 has the fake and vice versa. If they are the same weight then I know the last group, group3 has it. From there I follow the same logic just with the individual gold bars. I compare the first bar in the group to the second bar. If the first weighs less then that is the fake but if the second weighs less then that is the answer. If they are equal then the last bar in the group is the fake. This ensures that every time the application is ran the fake gold bar will be found in exactly two weighings.
--- scale/src/main/java/com/weightscale/ScaleClass.java
+++ scale/src/main/java/com/weightscale/ScaleClass.java
@@ -1,25 +1,112 @@
 package com.weightscale;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ScaleClass {
+    
+    private static WebDriver driver = new ChromeDriver();
+    private static String[] group1 = {"0", "1", "2"};
+    private static String[] group2 = {"3", "4", "5"};
+    private static String[] group3 = {"6", "7", "8"};
 
-    static public void driver_Start(WebDriver driver) throws InterruptedException {
-        driver.manage().window().fullscreen();
-        driver.get("http://sdetchallenge.fetch.com/");
-        driver.wait(1000);
-        driver_Close(driver);
-    }
+    static private String scaleLogic() throws InterruptedException {
 
-    static public void driver_Close(WebDriver driver) {
-        driver.close();
+        initialInput();
+        clickWeigh();
+
+        final String comparisonResult = getComparisonResult();
+        String finalAnswer = "";
+
+        /*  The button for the symbol and the button for resetting the grid have the same IDs and class names,
+        since this is lower in the hierarchy, I got both elements with the IDs and chose the second since it 
+        will always be the reset button*/
+        driver.findElements(By.id("reset")).get(1).click();
+        Thread.sleep(2000);
+
+        if(comparisonResult.contains("<")) {
+
+            finalAnswer = numberComparison(group1);
+        } 
+        else if (comparisonResult.contains(">")) {
+
+            finalAnswer = numberComparison(group2);
+        } 
+        else {
+            
+            finalAnswer = numberComparison(group3);
+        }
+
+        return finalAnswer;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        System.out.println("hello");
-        driver_Start(driver);
+        driver.manage().window().fullscreen();
+        driver.get("http://sdetchallenge.fetch.com/");
+        
+        // scalelogic returns the answer and then the correct button is clicked
+        driver.findElement(By.id("coin_" + scaleLogic())).click();
+
+        System.out.println(driver.switchTo().alert().getText());
+
+        driver.switchTo().alert().accept();
+
+        // The only ordered list on the page is the one in the game-info class so that is why I used this xpath
+        System.out.println(driver.findElement(By.xpath("//ol")).getText());
+
+        // Safely close the webdriver instance
+        driver.quit();
+    }
+
+    // Helper Functions
+
+    // Gets the '<', '>', or '=' string from the button in the middle of the grids
+    static private String getComparisonResult() {
+        return driver.findElement(By.id("reset")).getText();
     }
     
+    // Inserts numbers into the grid, just need to know the ID of the boxes
+    static private void numberInput(String elementID, String value) {
+        driver.findElement(By.id(elementID)).sendKeys(value);
+    }
+
+    static private void clickWeigh() throws InterruptedException {
+        driver.findElement(By.id("weigh")).click();
+
+        // I noticed the game info takes time to load so I added a delay in the thread.
+        Thread.sleep(2000);
+    }
+
+    static private void initialInput() {
+
+        // Inputs the first group on the left and second on the right
+        numberInput("left_0", group1[0]);
+        numberInput("left_1", group1[1]);
+        numberInput("left_2", group1[2]);
+        numberInput("right_0", group2[0]);
+        numberInput("right_1", group2[1]);
+        numberInput("right_2", group2[2]);
+    }
+
+    // This is similar to the logic inside of the scaleLogic function, this is just altered to
+    // take any group as a parameter and just compare individual numbers instead of the whole group
+    static private String numberComparison(String[] group) throws InterruptedException {
+        
+        numberInput("left_0", group[0]);
+        numberInput("right_0", group[1]);
+        clickWeigh();
+
+        Thread.sleep(2000);
+
+        String comparisonResult = getComparisonResult();
+
+        if(comparisonResult.contains("<")) {
+            return group[0];
+        } else if (comparisonResult.contains(">")) {
+            return group[1];
+        } else {
+            return group[2];
+        }
+    }
 }
